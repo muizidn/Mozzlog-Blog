54007572b7eb91bdc08b2d6292b15bd2e1d78b68
Muhammad Muizzsuddin <muiz.idn@gmail.com>
2023-06-26 18:30:55 +0700

    Update contact page design

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 export const metadata = {
   title: 'Contact',
   description: 'You can contact us!',
@@ -8,7 +10,7 @@ const contributors = [
   {
     name: 'Muhammad Muizzsuddin',
     github: 'https://github.com/muizidn',
-    stack: ['iOS', 'Flutter'],
+    stack: ['iOS', 'Flutter', 'ReactNative', 'NextJS', 'and so on'],
     role: 'Team Lead',
   },
 ];
@@ -18,23 +20,34 @@ export default function ContactPage() {
   for (var i = 0; i < contributors.length; i++) {
     const contributor = contributors[i];
     contributorsEl.push(
-      <h3 key={i.toString()} className="mt-12 text-center text-3xl">
-        {contributor.name} ({contributor.github}) as {contributor.role} Does{' '}
-        {contributor.stack}
-      </h3>
+      <div key={i.toString()}>
+        <h3 className="m-2 text-start text-3xl">{contributor.name}</h3>
+        <h4 className="m-2 text-start text-xl">As {contributor.role}</h4>
+        <h4 className="m-2 text-start text-xl">
+          Does {contributor.stack.join(', ')}
+        </h4>
+        <Link href={contributor.github} className='flex space-x-2 m-2 text-start text-xl'>
+          <h4>
+            Github
+          </h4>
+          <div className='text-blue-600'>{contributor.github}</div>
+        </Link>
+      </div>
     );
   }
   return (
     <>
       <h1 className="mt-12 text-center text-3xl font-bold">Contact</h1>
       <h2 className="mt-12 text-center text-3xl">
-        Hi call me Mozz. I am built by team of developers
+        {"Hello there. You wan't to talk? Let's talk!"}
       </h2>
       <h2 className="mt-12 text-center text-3xl">
-        But you can reach these amazing talents who contributed continuously
+        You can reach these amazing talents who are contributing
+        continuously.
       </h2>
-      {/* Enter you github link here */}
-      <tbody>{contributorsEl}</tbody>
+      <div className="mt-12">
+        <tbody>{contributorsEl}</tbody>
+      </div>
     </>
   );
 }
