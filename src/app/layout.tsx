805fddc330f37770a090ed8111c1bf7f2056e870
Muhammad Muizzsuddin <muiz.idn@gmail.com>
2023-07-07 08:45:23 +0700

    Add Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import Script from 'next/script';
+
 import 'katex/dist/katex.min.css';
 import 'prismjs/themes/prism-tomorrow.css';
 import 'react-notion-x/src/styles.css';
@@ -22,6 +24,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      {/* https://enlear.academy/add-google-analytics-to-a-next-js-application-5525892844db */}
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
       <body className="text-primary bg-primary relative mx-auto mb-20 flex w-full max-w-screen-xl flex-col px-[10vw] md:px-[5vw]">
         <Provider>
           <Header />
