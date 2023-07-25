e747f9edf06fda6f89291f1325b1b0c72e267608
Muhammad Muizzsuddin <muiz.idn@gmail.com>
2023-07-25 16:54:45 +0700

    Use SITE_URL environment value to replace https://mozzlog.com

--- src/components/blog-comment.tsx
+++ src/components/blog-comment.tsx
@@ -11,7 +11,7 @@ const BlogComments = ({ post }: { post: Post }) => {
   }
   const disqusShortname = 'mozzlog';
   const disqusConfig = {
-    url: 'https://mozzlog.com/blog/' + post.slug,
+    url: `${process.env.SITE_URL}/blog/` + post.slug,
     identifier: post.id, // Single post id
     title: post.title, // Single post title
   };
