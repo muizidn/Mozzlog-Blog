696aca1f2165df4af596e5068b12f85f8686be51
Muhammad Muizzsuddin <muiz.idn@gmail.com>
2023-07-27 06:22:35 +0700

    Use environment variable for CORS in next config

--- next.config.js
+++ next.config.js
@@ -39,7 +39,7 @@ const nextConfig = {
             source: "/mp/:path*",
             headers: [
                 { key: "Access-Control-Allow-Credentials", value: "true" },
-                { key: "Access-Control-Allow-Origin", value: "https://www.mozzlog.com" },
+                { key: "Access-Control-Allow-Origin", value: process.env.SITE_URL },
                 { key: "Access-Control-Allow-Methods", value: "POST" },
                 { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
             ]
