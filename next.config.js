98ad85ade9a0276cff9018f9bc74f2e7caf8e2a3
Muhammad Muizzsuddin <muiz.idn@gmail.com>
2023-07-26 20:46:19 +0700

    Add cross origin header in next.config.js for /mp/ endpoint

--- next.config.js
+++ next.config.js
@@ -33,6 +33,20 @@ const nextConfig = {
     return config;
   },
 
+  async headers() {
+    return [
+        {
+            source: "/mp/:path*",
+            headers: [
+                { key: "Access-Control-Allow-Credentials", value: "true" },
+                { key: "Access-Control-Allow-Origin", value: "https://www.mozzlog.com" },
+                { key: "Access-Control-Allow-Methods", value: "POST" },
+                { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+            ]
+        }
+    ]
+  },
+
   async rewrites() {
     return [
       {
