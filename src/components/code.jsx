29c710650ce7bd0da08819e4a5da58c5ea15b795
Muhammad Muizzsuddin <muiz.idn@gmail.com>
2023-07-28 07:31:05 +0700

    [#9] Implement C syntax highlighting

--- src/components/code.jsx
+++ src/components/code.jsx
@@ -4,6 +4,7 @@ const Code = dynamic(() =>
   import('react-notion-x/build/third-party/code').then(async (m) => {
     await Promise.all([
       import('prismjs/components/prism-swift.js'),
+      import('prismjs/components/prism-c.js'),
       ]);
     return m.Code;
   })
