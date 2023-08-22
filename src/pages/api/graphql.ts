3f75101b8085f27ce6e14f9c08a912451fda1f3d
Muhammad Muizzsuddin <muiz.idn@gmail.com>
2023-08-22 08:40:50 +0700

    Implement Query with Post schema

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -2,17 +2,57 @@ import { ApolloServer } from '@apollo/server';
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
 import { gql } from 'graphql-tag';
 
+// Sample data
+const posts = [
+  {
+    id: '1',
+    slug: 'post-1',
+    title: 'Sample Post 1',
+    categories: ['Category 1'],
+    cover: 'cover-url-1',
+    date: '2023-08-22',
+    published: true,
+    lastEditedAt: 1629638400000,
+  },
+  // ... add more posts
+];
+
+interface Post {
+  id: string;
+  slug: string;
+  title: string;
+  categories: string[];
+  cover: string | null;
+  date: string;
+  published: boolean;
+  lastEditedAt: number;
+}
+
 const resolvers = {
   Query: {
-    hello: () => 'worldss',
-    salazar: () => 'slytherin',
+    getPost: (parent: any, args: { id: string }) => {
+      const { id } = args;
+      return posts.find((post: Post) => post.id === id);
+    },
+    getAllPosts: () => posts,
   },
 };
 
 const typeDefs = gql`
+  type Post {
+    id: ID!
+    slug: String!
+    title: String!
+    categories: [String!]!
+    cover: String
+    date: String!
+    published: Boolean!
+    lastEditedAt: Int!
+  }
+
   type Query {
-    hello: String
-    salazar: String
+    getPost(id: ID!): Post
+    getAllPosts: [Post!]!
   }
 `;
 
