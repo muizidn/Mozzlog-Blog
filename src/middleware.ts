8601481e196d81ffea22cb358a4f29495e2bcc8c
Muhammad Muizzsuddin <muiz.idn@gmail.com>
2023-08-29 11:48:05 +0700

    Enable middleware auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,9 +4,9 @@ import type { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
   const password = request.nextUrl.searchParams.get('password');
 
-  // if (password !== process.env.API_SECRET) {
-  //   return NextResponse.json({ message: 'Wrong password' }, { status: 403 });
-  // }
+  if (password !== process.env.API_SECRET) {
+    return NextResponse.json({ message: 'Wrong password' }, { status: 403 });
+  }
 
   return NextResponse.next();
 }
