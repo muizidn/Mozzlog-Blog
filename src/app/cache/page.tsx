392f92f59542febef2c338e2d707d1dff306f243
Muhammad Muizzsuddin <muiz.idn@gmail.com>
2023-08-29 11:00:04 +0700

    Remove /cache page

--- src/app/cache/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getAllPosts } from '@/services/posts';
-
-export default async function CachePage() {
-  const allPosts = await getAllPosts();
-
-  return <div id="posts" data-posts={JSON.stringify(allPosts)} />;
-}
