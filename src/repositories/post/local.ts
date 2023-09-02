5dd5c596181958ebb6c9e1f2b5fa330bd90831bf
Muhammad Muizzsuddin <muiz.idn@gmail.com>
2023-09-02 10:39:29 +0700

    Move posts functions to local repo

--- src/repositories/post/local.ts
+++ src/repositories/post/local.ts
@@ -53,4 +53,102 @@ export default class PostgreLocalPostRepository {
             throw new Error(`Error updating or inserting post`);
         }
     }
+
+    // FIX ME
+
+    async getAllPosts() {
+        try {
+            const result = await sql`SELECT * FROM posts WHERE published = true`;
+
+            return result.rows.map((r) => ({
+                id: r.id,
+                slug: r.slug,
+                title: r.title,
+                categories: r.categories,
+                cover: r.cover,
+                date: (r.date as Date).toDateString(),
+                published: r.published,
+                lastEditedAt: Number(r.lasteditedat),
+            }));
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            throw new Error(`Error fetching posts`);
+        }
+    }
+
+    async getAllPostCategories(): Promise<string[]> {
+        try {
+            const result = await sql`
+                SELECT DISTINCT UNNEST(categories) as category
+                FROM posts
+                WHERE published = true
+            `;
+
+            return result.rows.map((r) => r.category).sort();
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            throw new Error(`Error fetching post categories`);
+        }
+    }
+
+    async getAllPostsSlugs(): Promise<string[]> {
+        try {
+            const result = await sql`SELECT slug FROM posts WHERE published = true`;
+
+            return result.rows.map((r) => r.slug);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            throw new Error(`Error fetching slugs`);
+        }
+    }
+
+    async getPostWithSlug(slug: string): Promise<Post | undefined> {
+        try {
+            const result = await sql`SELECT * FROM posts WHERE published = true AND slug = ${slug}`;
+
+            if (result.rows.length > 0) {
+                const r = result.rows[0];
+                return {
+                    id: r.id,
+                    slug: r.slug,
+                    title: r.title,
+                    categories: r.categories,
+                    cover: r.cover,
+                    date: (r.date as Date).toDateString(),
+                    published: r.published,
+                    lastEditedAt: Number(r.lasteditedat),
+                };
+            } else {
+                return undefined;
+            }
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            throw new Error(`Error fetching post with slug ${slug}`);
+        }
+    }
+
+    async getRelatedPosts(post: Post): Promise<Post[]> {
+        try {
+            const result = await sql`
+                  SELECT * FROM posts
+                  WHERE published = true
+                  AND slug != ${post.slug}
+                  AND categories && ${`{${post.categories.join(',')}}`}
+              `;
+
+            return result.rows.map((r) => ({
+                id: r.id,
+                slug: r.slug,
+                title: r.title,
+                categories: r.categories,
+                cover: r.cover,
+                date: r.date,
+                published: r.published,
+                lastEditedAt: Number(r.lasteditedat),
+            }));
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            throw new Error(`Error fetching related posts for ${post.slug}`);
+        }
+    }
 }
--- src/services/posts.ts
+++ src/services/posts.ts
@@ -1,98 +1,24 @@
+import PostgreLocalPostRepository from '@/repositories/post/local';
 import { Post } from '@/types/post';
-import { sql } from '@vercel/postgres';
+
+const repo = new PostgreLocalPostRepository()
 
 export async function getAllPosts() {
-  try {
-    const result = await sql`SELECT * FROM posts WHERE published = true`;
-
-    return result.rows.map((r) => ({
-      id: r.id,
-      slug: r.slug,
-      title: r.title,
-      categories: r.categories,
-      cover: r.cover,
-      date: (r.date as Date).toDateString(),
-      published: r.published,
-      lastEditedAt: Number(r.lasteditedat),
-    }));
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    throw new Error(`Error fetching posts`);
-  }
+  return repo.getAllPosts()
 }
 
 export async function getAllPostCategories(): Promise<string[]> {
-  try {
-      const result = await sql`
-          SELECT DISTINCT UNNEST(categories) as category
-          FROM posts
-          WHERE published = true
-      `;
-
-      return result.rows.map((r) => r.category).sort();
-  } catch (error) {
-      console.error('Error fetching data:', error);
-      throw new Error(`Error fetching post categories`);
-  }
+  return repo.getAllPostCategories();
 }
 
 export async function getAllPostsSlugs(): Promise<string[]> {
-  try {
-    const result = await sql`SELECT slug FROM posts WHERE published = true`;
-
-    return result.rows.map((r) => r.slug);
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    throw new Error(`Error fetching slugs`);
-  }
+  return repo.getAllPostsSlugs();
 }
 
 export async function getPostWithSlug(slug: string): Promise<Post | undefined> {
-  try {
-    const result = await sql`SELECT * FROM posts WHERE published = true AND slug = ${slug}`;
-
-    if (result.rows.length > 0) {
-      const r = result.rows[0];
-      return {
-        id: r.id,
-        slug: r.slug,
-        title: r.title,
-        categories: r.categories,
-        cover: r.cover,
-        date: (r.date as Date).toDateString(),
-        published: r.published,
-        lastEditedAt: Number(r.lasteditedat),
-      };
-    } else {
-      return undefined;
-    }
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    throw new Error(`Error fetching post with slug ${slug}`);
-  }
+  return repo.getPostWithSlug(slug)
 }
 
 export async function getRelatedPosts(post: Post): Promise<Post[]> {
-  try {
-    const result = await sql`
-            SELECT * FROM posts
-            WHERE published = true
-            AND slug != ${post.slug}
-            AND categories && ${`{${post.categories.join(',')}}`}
-        `;
-
-    return result.rows.map((r) => ({
-      id: r.id,
-      slug: r.slug,
-      title: r.title,
-      categories: r.categories,
-      cover: r.cover,
-      date: r.date,
-      published: r.published,
-      lastEditedAt: Number(r.lasteditedat),
-    }));
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    throw new Error(`Error fetching related posts for ${post.slug}`);
-  }
+  return repo.getRelatedPosts(post);
 }
