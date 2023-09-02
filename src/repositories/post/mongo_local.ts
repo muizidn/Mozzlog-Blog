843a4f2eef92375b9701ec8a3c7c0758cc8ca611
Muhammad Muizzsuddin <muiz.idn@gmail.com>
2023-09-02 14:12:20 +0700

    Fix agregation logic to get all categories

--- src/repositories/post/mongo_local.ts
+++ src/repositories/post/mongo_local.ts
@@ -103,13 +103,14 @@ export default class MongoLocalPostRepository {
             const collection = this.db.collection("posts");
             const pipeline = [
                 { $match: { published: true } },
+                { $project : { _id : 0, categories : 1 } },
                 { $unwind: "$categories" },
-                { $group: { id: "$categories" } },
-                { $sort: { id: 1 } },
+                { $group: { _id: "$categories" } },
+                { $sort: { _id: 1 } },
             ];
             const result = await collection.aggregate(pipeline).toArray();
 
-            return result.map((r) => r.id);
+            return result.map((r) => r._id);
         } catch (error) {
             console.error('Error fetching data:', error);
             throw new Error(`Error fetching post categories`);
