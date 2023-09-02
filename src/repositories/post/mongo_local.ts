7ba6cbfb63b5e967e5ebf19fcf6ca02afe1ae965
Muhammad Muizzsuddin <muiz.idn@gmail.com>
2023-09-02 13:51:37 +0700

    Fix projection using .project method

--- src/repositories/post/mongo_local.ts
+++ src/repositories/post/mongo_local.ts
@@ -120,7 +120,7 @@ export default class MongoLocalPostRepository {
         try {
             const collection = this.db.collection("posts");
             const filter = { published: true };
-            const result = await collection.find(filter, { projection: { slug: 1, id: 0 } }).toArray();
+            const result = await collection.find(filter).project({ slug: 1, _id: 0}).toArray();
 
             return result.map((r) => r.slug);
         } catch (error) {
