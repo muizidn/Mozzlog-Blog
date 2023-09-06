249f44a9844dd9255b6ceb2a9aa4bf92c6bbbf3d
Muhammad Muizzsuddin <muiz.idn@gmail.com>
2023-09-06 18:33:59 +0700

    Remove mixpanel-browser, use custom tracking api instead

--- src/services/analytics.ts
+++ src/services/analytics.ts
@@ -1,30 +1,24 @@
-// Remove { Dict, Query } if not using TypeScript
-import mixpanel, { Dict, Query } from "mixpanel-browser";
-
-const isProd = process.env.NODE_ENV === "production";
-
-mixpanel.init(`${process.env.NEXT_PUBLIC_MIXPANEL_TOKEN}`, {
-  api_host: `${process.env.NEXT_PUBLIC_ANALYTICS_API_URL}/mp`,
-});
+import axios from 'axios';
 
 export const Analytics = {
   identify: (id: string) => {
-    mixpanel.identify(id);
   },
   alias: (id: string) => {
-    mixpanel.alias(id);
   },
-  track: (name: string, props?: Dict) => {
-    mixpanel.track(name, props);
-  },
-  track_links: (query: Query, name: string) => {
-    mixpanel.track_links(query, name, {
-      referrer: document.referrer,
-    });
-  },
-  people: {
-    set: (props: Dict) => {
-      mixpanel.people.set(props);
-    },
+  track: (name: string, props?: { [key: string]: any }) => {
+    const apiUrl = `${process.env.NEXT_PUBLIC_ANALYTICS_API_URL}/api/track`;
+
+    const eventData = {
+      event: name,
+      properties: props || {},
+    };
+
+    axios.post(apiUrl, eventData)
+      .then((response) => {
+        console.log('Event tracked successfully:', response.data);
+      })
+      .catch((error) => {
+        console.error('Error tracking event:', error);
+      });
   },
 };
