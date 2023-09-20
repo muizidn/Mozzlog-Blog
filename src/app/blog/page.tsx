47b3bfbf7a75a05a155b48020a7b265fb2eff7fa
Muhammad Muizzsuddin <muiz.idn@gmail.com>
2023-09-20 19:48:05 +0700

    Add banner project in blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,4 @@
+import BannerProject from './[slug]/banner_project';
 import CategoryFilter from '@/components/filter/category-filter';
 import SearchBar from '@/components/filter/search-bar';
 import PostsGrid from '@/components/posts/posts-grid';
@@ -5,7 +6,8 @@ import { getAllPosts, getAllPostCategories } from '@/services/posts';
 
 export const metadata = {
   title: 'Blog',
-  description: 'Find you interesting posts that can help you finish your tasks faster.',
+  description:
+    'Find you interesting posts that can help you finish your tasks faster.',
 };
 
 export default async function BlogPage() {
@@ -16,6 +18,15 @@ export default async function BlogPage() {
     <>
       <section className="mb-8 mt-0 space-y-2 md:mt-8">
         <SearchBar />
+        <BannerProject url="https://www.tomioai.com/">
+          <div className="flex flex-col items-end">
+            <div>
+              Try Free Preview Access to TomioAI, enjoy prompt library for more
+              productive LLM experience. ↩️
+            </div>
+            <div>Go to https://tomioai.com</div>
+          </div>
+        </BannerProject>
         <CategoryFilter allCategories={allCategories} />
       </section>
       <PostsGrid allPosts={allPosts} />
