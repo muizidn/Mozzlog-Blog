2f646640cf34a1d3e5c18e7dc49bd00ec108d9d9
Muhammad Muizzsuddin <muiz.idn@gmail.com>
2023-09-20 20:01:43 +0700

    Fix banner project

--- src/app/blog/[slug]/banner_project.tsx
+++ src/app/blog/[slug]/banner_project.tsx
@@ -2,20 +2,17 @@ import Link from 'next/link';
 import React from 'react';
 
 interface BannerProjectProps {
-    url: string;
-    children: React.ReactNode;
+  url: string;
+  children: React.ReactNode;
 }
 
-const BannerProject: React.FC<BannerProjectProps> = ({url, children}) => {
+const BannerProject: React.FC<BannerProjectProps> = ({ url, children }) => {
   return (
-    <Link
-      href={url}
-      target="_blank"
-      rel="noopener noreferrer"
-      className="bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 border-dashed border-purple-800 rounded-lg p-4 text-white hover:opacity-75"
-    >
-      {children}
-    </Link>
+    <div className="w-fit rounded-lg border-dashed border-purple-800 bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 p-4 text-white hover:opacity-75">
+      <Link href={url} target="_blank" rel="noopener noreferrer">
+        {children}
+      </Link>
+    </div>
   );
 };
 
